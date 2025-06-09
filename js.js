00153fe92b774da4bf1691ccdd2a72a827f4409b
Noah <1113363@apps.nsd.org>
2025-06-09 16:40:03 +0000

    1

--- js.js
+++ js.js
@@ -1,12 +1,86 @@
 var Temp = document.getElementById("temp");
-var Card = Temp.querySelector(".card");
+var CARD = Temp.querySelector(".card");
 var Target = document.getElementById("target");
 
 
-for(var i = 0; i < 5; i++){
-    var carCopy = Card.cloneNode(true);
+for(var i = 0; i < 52; i++){
+    var cardCopy = CARD.cloneNode(true);
     cardCopy.innerHTML = "silver:" + i;
-    cardCopy.style.backroundPositionX = -i + "00%";
-    cardCopy.style.backroundPositionY = -Math.floor(i/13) + "00%";
+    cardCopy.style.backgroundPositionX = -(i)*100+"%";
+    cardCopy.style.backgroundPositionY = Math.floor(i/-13)*99+2+"%";
     Target.append(cardCopy);
 }
+class Game{
+    construtor(){
+        this.state      = {};
+        this.players    = players;
+        this.deck       = deck;
+        
+        this.play();
+
+    }
+
+    play = function(){
+        alert("Weeeeeee this is fun");
+    }
+}
+
+class Player{
+    constructor(name){
+        this.name = name;
+        this.hand = [];
+    }
+    draw(){
+    }
+}
+class Deck{
+
+    constructor(num){
+        this.carCount = num;
+        this.cards    = [];
+        this.make();
+    }
+
+    make(){
+        
+        for(var i = 0; i < this.cardCount; i++){
+            var card = new Card(i);
+            this.cards.push(card);
+        }
+
+        return this.cards;
+    }
+    shuffle(){
+
+    }
+
+}
+class Card{
+    constructor(num){
+        this.id         = num;
+        this.suit       = Math.floor(num/13);
+        this.val        = num % 13 + 1;
+        this.background = "Cards.png";
+        this.suits      = ["H", "C", "S", "D"];
+    }
+
+    getSuit(){
+        return suit;
+    }
+
+    getDom(){
+        var cardCopy = CARD.cloneNode(true);
+
+        cardCopy.innerHTML = "silver:" + this.id;
+        cardCopy.style.backgroundPositionX = -(this.id)*100+"%";
+        cardCopy.style.backgroundPositionY = Math.floor(this.id/-13)*99+2+"%";
+
+        return cardCopy;
+    }
+}
+
+
+var playerz = [];
+playerz[0] = new Player("bill");
+var marieAntoinet = new Deck(52);
+new Game(playerz, marieAntoinet);
